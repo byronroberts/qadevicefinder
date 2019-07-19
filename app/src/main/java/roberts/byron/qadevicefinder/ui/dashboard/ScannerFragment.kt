4231612d71e9494ec1455e69a842dcf23f2797b6
byron.roberts <byron.roberts@autotrader.co.uk>
2019-07-19 15:46:20 +0100

    First commit

---
+++ app/src/main/java/roberts/byron/qadevicefinder/ui/dashboard/ScannerFragment.kt
@@ -0,0 +1,38 @@
+package roberts.byron.qadevicefinder.ui.dashboard
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.zxing.Result
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+class ScannerFragment : Fragment(), ZXingScannerView.ResultHandler {
+
+    private lateinit var scannerView: ZXingScannerView
+
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        scannerView = ZXingScannerView(activity)
+        return scannerView
+    }
+
+    override fun onResume() {
+        super.onResume()
+        scannerView.setResultHandler(this)
+        scannerView.startCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        scannerView.stopCamera()
+    }
+
+    override fun handleResult(rawResult: Result?) {
+        Log.d("BARCODE", "Barcode captured")
+        scannerView.resumeCameraPreview(this)
+    }
+
+
+}
